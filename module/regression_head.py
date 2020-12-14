added gradient checking after clip norm; fixed normalization checking in regression_head.py

module/regression_head.py
@@ -58,12 +58,10 @@ class RegressionHead(nn.Module):
 
         # compute sum of attention
         norm = attn_weight_rw.sum(-1, keepdim=True)
-        if occ_mask is None:
-            norm[norm < 0.1] = 1.0
-        else:
+        if occ_mask is not None:
             norm[occ_mask, :] = 1.0  # set occluded region norm to be 1.0 to avoid division by 0
-            # TODO: for debugging only
-            # norm[norm < 0.1] = 1.0
+        norm[norm < 0.1] = 1.0
+        print("debugging norm", norm.max().item(), norm.min().item())
 
         # re-normalize to 1
         attn_weight_rw = attn_weight_rw / norm  # re-sum to 1
@@ -245,6 +243,7 @@ class RegressionHead(nn.Module):
         else:
             # softmax
             attn_ot = self._softmax(attn_weight)
+        output['attn_ot'] = attn_ot
 
         # compute relative response (RR) at ground truth location
         if x.disp is not None:

utilities/train.py
@@ -46,7 +46,11 @@ def train_one_epoch(model: torch.nn.Module, data_loader: Iterable, optimizer: to
 
         # clip norm
         if max_norm > 0:
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+            total_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+            if torch.isnan(total_norm):
+                print('gradient nan, skipping current step')
+                torch.cuda.empty_cache()
+                continue
 
         # step optimizer
         optimizer.step()